3903a237b9717538aa9fbe2660b32124575fc002
BJKim <xcxcmath@icloud.com>
2017-11-05 18:22:49 +0900

    Font changed

--- gamestatus.js
+++ gamestatus.js
@@ -2,11 +2,11 @@ function gamestatus(){
     this.turn = "A"; // A or B
     this.clickable = false;
     this.phase = "D"; // D phase or A phase
-    this.message = "게임을 시작합니다"; //Message above the map
+    this.message = "Game start"; //Message above the map
     this.draw = function(){
         context.beginPath();
         context.fillStyle = forecolor;
-        context.font = "30px Arial";
+        context.font = "30px san-serif";
         context.textAlign = "center";
         context.fillText(this.message, canvas.width / 2, 40, 800);
         context.closePath();
