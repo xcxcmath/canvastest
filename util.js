b3598409e9ddfb93ce634d0567e0cbd9d63db4c0
Ohjun Kwon <kojandy@gmail.com>
2017-11-26 18:26:09 +0900

    Implement hamiltonian possibility checker function

--- util.js
+++ util.js
@@ -8,3 +8,38 @@ function isHamiltonian(vertices, edges, start, end) {
     }
     return vertices.length === 0;
 }
+
+function isAdjacent(here, there) {
+    return edge_info[here][there] === 1;
+}
+
+function adjacentPoints(points, here) {
+    var aa = [];
+    points.forEach(function (point) {
+        if (isAdjacent(point, here)) {
+            aa.push(point);
+        }
+    });
+    return aa;
+}
+
+function isPossible(points, path, end) {
+    var aa = [];
+    points.forEach(function (point) {
+        var test = path.some(function (v) {
+            return point === v;
+        });
+        if (!test) {
+            aa.push(point);
+        }
+    });
+    return _isPossible(aa, path[path.length - 1], end);
+}
+
+function _isPossible(points, start, end) {
+    if (points.length === 0) return start === end;
+    var adjacent = adjacentPoints(points, start);
+    return adjacent.some(function (v) {
+        return isPossible(points, [start, v], end);
+    });
+}
